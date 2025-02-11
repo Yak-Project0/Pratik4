e6578fbb4da654bd0b18319ea440ac67e0b12ec0
Yak-Project0 <yildirayalikara0@gmail.com>
2025-02-11 13:16:37 +0300

    Km başında düşen miktar değişken olarak tanımlandı daha sonra değiştirilme olasılığı göz önünde bulundurularak.(perKm=Km başına düşen miktar)

--- src/Main.java
+++ src/Main.java
@@ -4,11 +4,12 @@ public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         double ucret;
-        int gidilenYolKM;
+        int km;
+        double perKm = 2.20;
         System.out.print("Yolcunun kaç km gittiğini giriniz: ");
-        gidilenYolKM = scanner.nextInt();
+        km = scanner.nextInt();
         ucret = 10;
-        ucret +=2.20*gidilenYolKM;
+        ucret +=perKm*km;
         ucret = ucret<20 && ucret>0 ? 20 : ucret;
         System.out.println("Ödemeniz gereken miktar: "+ucret);
     }
